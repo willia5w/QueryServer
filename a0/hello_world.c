fa166cb1c34a27065f07979f579bdaf51a622bb4
Dan Williams <williams.dan@husky.neu.edu>
2019-05-13 12:16:06 -0700

    Adding initial a0

---
+++ a0/hello_world.c
@@ -0,0 +1,25 @@
+/*
+ * CS 5007, Assignment 0
+ * Summer 2019
+ * Northeastern University, Seattle
+ *
+ * Author: Adrienne Slaughter
+ * 
+ * May 9, 2019
+ *
+ */
+#include "hello_world.h"
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	long a = -889262067;
+	long b = 330223330;
+
+    SayHello(a, b); 
+    return 0;
+}
+
+void SayHello(long foo, long bar){
+	printf("The magic code is: %X\n", foo + bar);
+}
